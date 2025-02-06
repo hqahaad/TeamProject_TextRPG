bba4856868a773cbf21e0533291736d3d9ed0aac
hqahaad <grabq04@gmail.com>
2025-02-06 17:33:05 +0900

    start commit

---
+++ TeamProject_TextRPG/IScene.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject_TextRPG
+{
+     public interface IScene
+     {
+          void Awake();
+          void Start();
+          void End();
+     }
+}
