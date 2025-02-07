20a18ae8d501741dd20b49010245b3589b159481
Sinneli <nyeliplays1911@gmail.com>
2025-02-07 11:58:23 +0900

    Battle Set Enemy 기능 추가
    랜덤하게 적 1~4마리 생성. 종류 상관 없이 랜덤 생성은 만들고 생각

--- TeamProject_TextRPG/BattleSystem/Battle.cs
+++ TeamProject_TextRPG/BattleSystem/Battle.cs
@@ -23,8 +23,16 @@ namespace TeamProject_TextRPG.BattleSystem
 
           public void SetEnemy(IBattler battler)
           {
-               playerList.Add(battler);
-               battleQueue.Enqueue(battler);
+           
+               Random rnd = new Random();
+               int num = rnd.Next(1, 5); // 1에서 사까지 생성
+               for (int i = 0; i < num; i++) // 루프 통해 1~4마리에 몬스터 추가 + 생성
+            {
+                enemyList.Add(battler);
+                battleQueue.Enqueue(battler);
+            }
+               
+               
           }
 
           public void StartBattle()
