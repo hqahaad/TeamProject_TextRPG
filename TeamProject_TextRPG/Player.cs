2971e47e892f823b3d4f27a2402eb8317631f3f6
hqahaad <grabq04@gmail.com>
2025-02-07 11:26:57 +0900

    feat: 배틀 메소드 변경

--- TeamProject_TextRPG/Player.cs
+++ TeamProject_TextRPG/Player.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeamProject_TextRPG.BattleSystem;
 
 namespace TeamProject_TextRPG
 {
-     public class Player
+     public class Player : IBattler
      {
           //수정
           int hp = 100;
           int att = 10;
+
+          public void Attack(IBattler battler)
+          {
+               throw new NotImplementedException();
+          }
+
+          public IBattler AttackCaster(List<IBattler> battler)
+          {
+               throw new NotImplementedException();
+          }
+
+          public void GetDamage(IDamageable damage)
+          {
+               throw new NotImplementedException();
+          }
+
+          public bool IsPlayer()
+          {
+               return true;
+          }
      }
 }
