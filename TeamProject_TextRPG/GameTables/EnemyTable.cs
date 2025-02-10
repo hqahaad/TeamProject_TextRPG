7e7010f1d77e49c03dbc9f09e8cc8f1471dfe6f6
leesiyen <leesiyen02012@naver.com>
2025-02-10 10:55:55 +0900

    refactory: enemy table

--- TeamProject_TextRPG/GameTables/EnemyTable.cs
+++ TeamProject_TextRPG/GameTables/EnemyTable.cs
@@ -11,8 +11,15 @@ namespace TeamProject_TextRPG.GameTables
           public EnemyTable() 
           {
                Table<Enemy>.Get()?.Add("미니언", new Enemy("미니언", 1, 10, 5, 5));
-
-               Table<Enemy>.Get().Load("미니언");
+               Table<Enemy>.Get()?.Add("대포미니언", new Enemy("대포미니언", 1, 10, 5, 5));
+               Table<Enemy>.Get()?.Add("공허충", new Enemy("공허충", 1, 10, 5, 5));
+               Table<Enemy>.Get()?.Add("슬라임", new Enemy("슬라임", 1, 10, 5, 5));
+               Table<Enemy>.Get()?.Add("걷는 버섯", new Enemy("걷는 버섯", 1, 10, 5, 5));
+               Table<Enemy>.Get()?.Add("거대전갈", new Enemy("거대전갈", 1, 10, 5, 5));
+               Table<Enemy>.Get()?.Add("땅거미", new Enemy("땅거미", 1, 10, 5, 5));
+               Table<Enemy>.Get()?.Add("바하무트", new Enemy("바하무트", 1, 10, 5, 5));
+               Table<Enemy>.Get()?.Add("도적", new Enemy("도적", 1, 10, 5, 5));
+               Table<Enemy>.Get()?.Add("지나가던 개발자", new Enemy("지나가던 개발자", 1, 10, 5, 5));
           }
      }
 }
