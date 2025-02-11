e6c71451d0c9f5fe4f71eac25ea924b3d46542ad
hqahaad <grabq04@gmail.com>
2025-02-11 21:16:20 +0900

    feat: 스킬 시스템 구현

--- TeamProject_TextRPG/SkillSystem/Skills/Skill_BattleCry.cs
+++ TeamProject_TextRPG/SkillSystem/Skills/Skill_BattleCry.cs
@@ -9,19 +9,30 @@ namespace TeamProject_TextRPG.SkillSystem.Skills
 {
     public class Skill_BattleCry : Skill
     {
+        public Skill_BattleCry()
+        {
+            skillName = "전투의 함성";
+            skillDescription = "모든 적을 공격합니다";
+        }
+
         public override void SkillAction(Battle battle)
         {
+            var enemys = battle.GetUnits(FactionType.Enemy);
 
+            foreach (var iter in enemys)
+            {
+                iter.GetDamage(new Damage(order.attackPower));
+            }
         }
 
         public override bool IsCostSkill()
         {
-            return false;
+            return order.mp < 100;
         }
 
         public override void UseCost()
         {
-            
+            order.mp -= 100;
         }
     }
 }
